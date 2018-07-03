b66f4aea3274887dc8476b407f590f6aa4ef90c6
Justin Quiros <justin@roboamp.com>
2018-07-03 13:34:52 -0500

    final commit

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default () => {
 	return [
-		{title: 'JavaScript: The Good Parts' },
-		{title: 'Harry Potter' },
-		{title: 'The Dark Tower' },
-		{title: 'Redux FOR Dummies' }
+		{title: 'JavaScript: The Good Parts', pages: 111 },
+		{title: 'Harry Potter', pages: 300 },
+		{title: 'The Dark Tower', pages: 211 },
+		{title: 'Redux FOR Dummies', pages: 99 }
 	]
 }
 
